4f8c5ade187efdb843b1d03dcabc11b400737185
westonslaughter <63979914+westonslaughter@users.noreply.github.com>
2020-07-29 12:22:38 -0700

    Complete basic CSS overhaul, blue and egg echeme

--- studysite/studysite/wsgi.py
+++ studysite/studysite/wsgi.py
@@ -8,12 +8,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 path = '/home/wesslaughter/CHIME/studysite'
 if path not in sys.path:
-sys.path.append(path)
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studysite.settings')
 
