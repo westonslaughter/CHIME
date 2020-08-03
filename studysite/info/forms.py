57e120ec1531399f7ebcddcdc6a392d3f49b6ccc
westonslaughter <63979914+westonslaughter@users.noreply.github.com>
2020-08-03 08:53:08 -0700

    Staff single contact primary key resolved

--- studysite/info/forms.py
+++ studysite/info/forms.py
@@ -5,10 +5,10 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactMod
         fields = {'name','email','comment'}
-        field_order = ['name','email','comment']
+        field_order = ['name','email','comment',]
 
         widgets ={
-            'name': forms.TextInput(attrs={'class': 'form-control','placeholder':'Your Name','style': 'width:270px'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control','placeholder':'Your Email','style': 'width:270px'}),
+            'name': forms.TextInput(attrs={'class': 'form-control','placeholder':'Your Name','style': 'width:270px;margin-bottom:.5rem;'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control','placeholder':'Your Email','style': 'width:270px; margin-bottom:.5rem;'}),
             'comment': forms.Textarea(attrs={'class': 'form-control','placeholder':"Tell us about yourself, and why you're interested in the study!"}),
         }
